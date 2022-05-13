edbe3d3eddf297a8209accdca5f8e1cdd0f406ed
basssara <zomboy.z77z@gmail.com>
2022-05-13 16:53:01 +0500

    changes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,14 @@ import React, { FC } from 'react'
 
 const PageNotFound: FC = () => {
     return (
-        <>
-            <h1>404</h1>
-            <h3>Page not Found</h3>
+        <><div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+                <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    404
+                </h1>
+                <h2 className="mt-6 text-center text-2xl text-gray-900">Page not Found</h2>
+            </div>
+        </div>
         </>
     )
 }
